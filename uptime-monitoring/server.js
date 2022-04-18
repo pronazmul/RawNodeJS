aad2d7b83c6f46e30f3b69e6204359dca55710d6
pronazmul <developernazmul@gmail.com>
2022-04-18 19:43:08 +0600

    uptime-monitoring basic Server Setup

---
+++ uptime-monitoring/server.js
@@ -0,0 +1,38 @@
+/*
+* Title: Raw Node Uptime Monitoring Application.
+* Description: User Can Create Accouts, Store Api Endpoints, and Monitor Uptime. Notified by SMS.
+* Author: Nazmul Huda
+* Date: 18/04/2022
+*/
+
+// Dependencies
+const http = require('http');
+
+// Module Scaffolding
+const app = {};
+
+//  Configuration
+app.config = {
+    port:5000,
+}
+
+// Create Server
+app.createServer = (callback)=> {
+    // Create Server
+    const server = http.createServer(callback);
+
+    // listen To Port
+    server.listen(app.config.port, ()=> {
+        console.log(`Server is listening on port ${app.config.port}`);
+    })
+}
+
+// Handle Request Response
+app.handleRequestResponse = (req, res)=> {
+    res.end("Welcome Programer")
+}
+
+
+// Start Server
+app.createServer(app.handleRequestResponse);
+
